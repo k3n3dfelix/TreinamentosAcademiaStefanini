c50a134fe4d6d8ce694c58c5d8763b7bcd5c7e50
Kened Silva Felix <ksfelix1@stefanini.com>
2022-04-07 17:07:32 -0300

    Revisão Java Fundamentos

---
+++ javaFundamentos/src/javaPoo/Carro.java
@@ -0,0 +1,19 @@
+package javaPoo;
+
+public class Carro {
+	int ano;
+	String cor;
+	
+	void ligar() {
+		System.out.println("engine On.........");
+	}
+	
+	void desligar() {
+		System.out.println("engine Off.........");
+	}
+	
+	void acelerar() {
+		System.out.println("vrummmm.........");
+	}
+
+}
