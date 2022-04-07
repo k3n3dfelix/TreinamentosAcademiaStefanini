cf8db8645bbe5e47094435bcf21cc00d69b596ea
Kened Silva Felix <ksfelix1@stefanini.com>
2022-04-07 11:42:06 -0300

    Revisão Fundamentos

---
+++ javaFundamentos/src/javaFundamentos/Fundamentos.java
@@ -0,0 +1,22 @@
+package javaFundamentos;
+
+public class Fundamentos {
+
+	public static void main(String[] args) {
+		String nome = "Kened";
+		int idade = 29;
+		char sexo = 'M';
+		double temperatura = 30.7;
+		boolean arCondicionado = false;
+		
+		
+		System.out.println("Nome: "+ nome);
+		System.out.println("Idade: "+ idade);
+		System.out.println("Sexo: "+ sexo);
+		System.out.println("Temperatura: "+ temperatura);
+		System.out.println("Ar Condicionado: "+ arCondicionado);
+
+
+	}
+
+}
